3852da6e74c296c81e92d4f2f946aa23c3f5667d
AboTalib Mahfoodh <ab.mahfoodh@gmail.com>
2018-01-10 15:55:38 -0800

    Clean up plus adding plot for training steps to show the model during training

--- NN1.py
+++ NN1.py
@@ -2,14 +2,13 @@
 # Import the relevant components
 from __future__ import print_function
 import numpy as np
-import sys
-import os
 import cntk as C
 import cntk.tests.test_utils
 import matplotlib.pyplot as plt
 from collections import defaultdict
 import math
-
+cntk.tests.test_utils.set_device_from_pytest_env() # (only needed for our build system)
+C.cntk_py.set_fixed_random_seed(1) # fix the random seed so that LR examples are repeatable
 # Ensure that we always get the same results
 np.random.seed(0)
 
@@ -37,21 +36,6 @@ def moving_average(a, w=10):
         return a[:]
     return [val if idx < w else sum(a[(idx-w):idx])/w for idx, val in enumerate(a)]
 
-def Plot_data():
-    plt.figure(1)
-    colors = ['r' if label == 0 else 'b' for label in labels[:, 0]]
-    plt.scatter(features[:, 0], features[:, 1], c=colors)
-    plt.xlabel("Age (scaled)")
-    plt.ylabel("Tumor size (in cm)")
-    plt.show(block = False)
-
-def linear_layer(input_var, output_dim):
-    input_dim = input_var.shape[0]
-    weight_param = C.parameter(shape=(input_dim, output_dim))
-    bias_param = C.parameter(shape=(output_dim))
-    mydict['w'], mydict['b'] = weight_param, bias_param
-    return C.times(input_var, weight_param) + bias_param
-
 # Print the training progress
 def print_training_progress(trainer, mb, frequency, verbose=1):
     training_loss, eval_error = "NA", "NA"
@@ -64,14 +48,26 @@ def print_training_progress(trainer, mb, frequency, verbose=1):
 
     return mb, training_loss, eval_error
 
-def Plot_training_error():
+def Plot_data():
+    plt.figure(1)
+    colors = ['r' if label == 0 else 'b' for label in labels[:, 0]]
+    plt.scatter(features[:, 0], features[:, 1], c=colors)
+    plt.xlabel("Age (scaled)")
+    plt.ylabel("Tumor size (in cm)")
+    plt.show(block = False)
+
+def plotCurrentModel(bias_vector, weight_matrix):
+    plt.plot([0, bias_vector[0] / weight_matrix[0][1]],
+             [bias_vector[1] / weight_matrix[0][0], 0], c='g', lw=3)
+
+def PlotTrainingError():
     global fig, y1, y2, li1, li2
     plot_data_size = int(num_minibatches_to_train / training_progress_output_freq)
     fig = plt.figure(2)
     ax1 = plt.subplot(211)
-    ax1.set_ylim([0, 2])
+    ax1.set_ylim([0, 1.5])
     ax2 = plt.subplot(212)
-    ax2.set_ylim([0, 0.4])
+    ax2.set_ylim([0, 0.6])
     # some X and Y data
     x = training_progress_output_freq * np.arange(plot_data_size)
     y1 = [2] * plot_data_size
@@ -94,7 +90,7 @@ def UpdateTrainingErrorPlot():
     li2.set_ydata(y2)
     fig.canvas.draw()
 
-def PlotTestDataResultsAndModel():
+def PlotTestDataResultsAndModel(bias_vector, weight_matrix):
     global label
     plt.figure(3)
     colors = ['r' if label == 0 else 'b' for label in labels[:, 0]]
@@ -105,27 +101,30 @@ def PlotTestDataResultsAndModel():
     plt.ylabel("Tumor size (in cm)")
     plt.show()
 
+def PlotTrainingDataAndModel():
+    plt.cla()
+    Plot_data()
+    plotCurrentModel(mydict['b'].value, mydict['w'].value)
+    plt.draw()
+    plt.waitforbuttonpress()
+
+def linear_layer(input_var, output_dim):
+    mydict = {}
+    input_dim = input_var.shape[0]
+    weight_param = C.parameter(shape=(input_dim, output_dim))
+    bias_param = C.parameter(shape=(output_dim))
+    mydict['w'], mydict['b'] = weight_param, bias_param
+    return C.times(input_var, weight_param) + bias_param, mydict
+
+
 # Define the network
 input_dim = 2
 num_output_classes = 2
 
-# Create the input variables denoting the features and the label data. Note: the input
-# does not need additional info on the number of observations (Samples) since CNTK creates only
-# the network topology first
-mysamplesize = 32
-features, labels = generate_random_data_sample(mysamplesize, input_dim, num_output_classes)
-
-# Plot the data
-Plot_data()
-
 feature = C.input_variable(input_dim, np.float32)
-# Define a dictionary to store the model parameters
-mydict = {}
-
-output_dim = num_output_classes
-z = linear_layer(feature, output_dim)
-
 label = C.input_variable(num_output_classes, np.float32)
+
+z, mydict = linear_layer(feature, num_output_classes)
 loss = C.cross_entropy_with_softmax(z, label)
 eval_error = C.classification_error(z, label)
 
@@ -144,11 +143,10 @@ num_minibatches_to_train = int(num_samples_to_train  / minibatch_size)
 training_progress_output_freq = 50
 plotdata = defaultdict(list)
 
-Plot_training_error()
+PlotTrainingError()
 
 for i in range(0, num_minibatches_to_train):
     features, labels = generate_random_data_sample(minibatch_size, input_dim, num_output_classes)
-
 
     # Assign the minibatch data to the input variables and train the model on the minibatch
     trainer.train_minibatch({feature : features, label : labels})
@@ -160,16 +158,12 @@ for i in range(0, num_minibatches_to_train):
         plotdata["loss"].append(loss)
         plotdata["error"].append(error)
         UpdateTrainingErrorPlot()
+        # PlotTrainingDataAndModel()
 
 # Run the trained model on a newly generated dataset
-test_minibatch_size = 50
+test_minibatch_size = 25
 features, labels = generate_random_data_sample(test_minibatch_size, input_dim, num_output_classes)
 
 trainer.test_minibatch({feature : features, label : labels})
 
-bias_vector   = mydict['b'].value
-weight_matrix = mydict['w'].value
-
-PlotTestDataResultsAndModel()
-
-plt.show()
+PlotTestDataResultsAndModel(mydict['b'].value, mydict['w'].value)
